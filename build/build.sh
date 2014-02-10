11b5c293fc5843fb1a457a58173a0871640cd0e2
mangui <g.du.pontavice@gmail.com>
2014-02-10 18:54:57 +0800

    fix build command line

--- build/build.sh
+++ build/build.sh
@@ -14,7 +14,7 @@ echo "Compiling HLSProviderChromeless.swf"
 $FLEXPATH/bin/mxmlc ../src/org/mangui/chromeless/ChromelessPlayer.as -source-path ../src -o ../test/chromeless/HLSProviderChromeless.swf -library-path+=../lib -use-network=false -optimize=true -incremental=true -target-player="11.1" -static-link-runtime-shared-libraries=true -default-size 480 270 -default-background-color=0x000000
 
 echo "Compiling HLSProviderFlowPlayer.swf"
-FLEXPATH/bin/mxmlc ../src/org/mangui/flowplayer/HLSPluginFactory.as -source-path ../src -o ../test/flowplayer/HLSProviderFlowPlayer.swf -library-path+=../lib -library-path+=../lib/flowplayer -use-network=false -optimize=true -incremental=true -target-player="11.1" -static-link-runtime-shared-libraries=true
+$FLEXPATH/bin/mxmlc ../src/org/mangui/flowplayer/HLSPluginFactory.as -source-path ../src -o ../test/flowplayer/HLSProviderFlowPlayer.swf -library-path+=../lib -library-path+=../lib/flowplayer -use-network=false -optimize=true -incremental=true -target-player="11.1" -static-link-runtime-shared-libraries=true
 
 echo "Compiling HLSProviderOSMF.swf"
 $FLEXPATH/bin/mxmlc ../src/org/mangui/osmf/plugins/HLSDynamicPlugin.as -source-path ../src -o ../test/osmf/HLSProviderOSMF.swf -library-path+=../lib -library-path+=../lib/osmf -externs org.osmf.net.httpstreaming.HTTPNetStream -use-network=false -optimize=true -incremental=true -target-player="10.1" -static-link-runtime-shared-libraries=true #-compiler.verbose-stacktraces=true -link-report=../test/osmf/link-report.xml

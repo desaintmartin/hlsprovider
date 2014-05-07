desaintmartin/hlsprovider

commit b957d913b1f6b04dcea27de3ab8d5ab8de74defe

HLSProvider/OSMF fix missing overlayPlay Button
regression introduced in v0.5.6
fix https://github.com/mangui/HLSprovider/issues/166

src/org/mangui/osmf/plugins/HLSTimeTrait.as
@@ -6,13 +6,25 @@ package org.mangui.osmf.plugins {
 
     public class HLSTimeTrait extends TimeTrait {
         private var _hls : HLS;
+        private var _duration : Number;
+        private var _position : Number;
 
         public function HLSTimeTrait(hls : HLS, duration : Number = 0) {
             Log.debug("HLSTimeTrait()");
             super(duration);
+            _position = 0;
+            _duration = duration;
             _hls = hls;
             _hls.addEventListener(HLSEvent.MEDIA_TIME, _mediaTimeHandler);
             _hls.addEventListener(HLSEvent.PLAYBACK_COMPLETE, _playbackComplete);
+        }
+
+        override public function get duration() : Number {
+            return _duration;
+        }
+
+        override public function get currentTime() : Number {
+            return _position;
         }
 
         override public function dispose() : void {
@@ -24,10 +36,8 @@ package org.mangui.osmf.plugins {
 
         /** Update playback position/duration **/
         private function _mediaTimeHandler(event : HLSEvent) : void {
-            var new_duration : Number = event.mediatime.duration;
-            var new_position : Number = Math.max(0, event.mediatime.position);
-            setDuration(new_duration);
-            setCurrentTime(new_position);
+            _position = Math.max(0, event.mediatime.position);
+            _duration = event.mediatime.duration;
         };
 
         /** playback complete handler **/

commit 598a251cbc9d8c91469dcc7c0548865f9d4d0ccf

fix duration not updated on GrindPlayer regression introduced by https://github.com/mangui/HLSprovider/commit/b957d913b1f6b04dcea27de3ab8d5ab8de74defe related to https://github.com/mangui/HLSprovider/issues/166 which is also fixed

src/org/mangui/osmf/plugins/HLSTimeTrait.as
@@ -6,25 +6,14 @@ package org.mangui.osmf.plugins {
 
     public class HLSTimeTrait extends TimeTrait {
         private var _hls : HLS;
-        private var _duration : Number;
-        private var _position : Number;
 
         public function HLSTimeTrait(hls : HLS, duration : Number = 0) {
             Log.debug("HLSTimeTrait()");
             super(duration);
-            _position = 0;
-            _duration = duration;
+            setCurrentTime(0);
             _hls = hls;
             _hls.addEventListener(HLSEvent.MEDIA_TIME, _mediaTimeHandler);
             _hls.addEventListener(HLSEvent.PLAYBACK_COMPLETE, _playbackComplete);
-        }
-
-        override public function get duration() : Number {
-            return _duration;
-        }
-
-        override public function get currentTime() : Number {
-            return _position;
         }
 
         override public function dispose() : void {
@@ -36,8 +25,10 @@ package org.mangui.osmf.plugins {
 
         /** Update playback position/duration **/
         private function _mediaTimeHandler(event : HLSEvent) : void {
-            _position = Math.max(0, event.mediatime.position);
-            _duration = event.mediatime.duration;
+            var new_duration : Number = event.mediatime.duration;
+            var new_position : Number = Math.max(0, event.mediatime.position);
+            setDuration(new_duration);
+            setCurrentTime(new_position);
         };
 
         /** playback complete handler **/